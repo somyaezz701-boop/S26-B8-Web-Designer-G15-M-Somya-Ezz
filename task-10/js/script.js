2ff9dda323e0d7ba67fd98d3e3112aaaac8d35f9
Somya Ezz <somyaezz701@gmail.com>
2026-08-30 10:36:37 +0300

    task10

---
+++ task-10/js/script.js
@@ -0,0 +1,19 @@
+var degree = prompt("Enter Your degree")
+if ( degree ==null || degree == ""){
+    alert('please enter your degree')
+}
+else if(degree>100){
+    console.log('invalid degree')
+}
+else if(degree>90){
+    console.log('Excellent')
+}
+else if(degree>80){
+    console.log('very good')
+}
+else if(degree>70){
+    console.log('good')
+}
+else{
+    console.log('failed')
+}
